33a398dd7a97d05b6c7b8aaec36daa90386bd4c4
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-02 17:36:11 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
-/**
-* Creating a C program
-* This program prints a string without the printf
-*/
-int main(void)
+/* This program prints a string without the printf */
+main()
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
