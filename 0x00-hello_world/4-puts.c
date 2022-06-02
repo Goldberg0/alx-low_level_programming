e51f5fa9751d5583933c27239edd935f7736ca87
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-02 17:17:48 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* Creating a C program
+* This program prints a string without the printf
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
