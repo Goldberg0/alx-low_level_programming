6fd334e1473c5974540ed0a5c09150de020dc603
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-02 20:22:50 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char yo [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+fprintf(stderr, "%c\n", yo);
 return (1);
 }
