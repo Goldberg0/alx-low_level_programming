b2e0635e9ad1cc6f8593af9cc39702ff809e531e
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-03 21:07:08 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints all base 16 numbers in lowercase
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
