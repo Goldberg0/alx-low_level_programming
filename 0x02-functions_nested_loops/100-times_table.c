052af3b1ba70beb9e5041028d18f0a50f235ccdb
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-08 12:35:38 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,18 +4,17 @@
  * @n: integer input
  * Return: void
  */
-
 void print_times_table(int n)
 {
 	int a, b, c, d, e, f;
-
 	if (n >= 0 && n < 15)
 	{
 		for (a = 0; a <=  n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
-				c = a * b; d = c / 100;
+				c = a * b;
+				d = c / 100;
 				e = (c /  10) % 10;
 				f = (c % 100) % 10;
 				if (b == 0)
