e20ce50897843e5250d02f50aacbf58cde9888c1
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-09 17:54:27 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers 0-9 with exception 2 and 4
+ * print_numbers - prints numbers 0-9
  *
  * Return: void
  */
 
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
