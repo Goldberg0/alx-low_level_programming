e7f973d01510f03b87c3c04e17c5656b0076de0b
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-10 02:57:18 +0100

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 01234567891011121314\n x10
+ *
+ * Return: void
+ */
+
+
+void more_numbers(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = '0'; c <='15'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
