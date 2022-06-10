4f26d8cd7eb77f205b8e677ec833955b9d9a35d6
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-10 05:31:19 +0100

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer with _putchar
+ * @n: integer to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	int a, b, c;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a *= -1; b *= -1; c *= -1
+	}
+	if (a != 0)
+    _putchar('0' + a)
+     (b != 0)
+    _putchar('0' + b)
+     (c != 0)
+    _putchar('0' + c)
+}
