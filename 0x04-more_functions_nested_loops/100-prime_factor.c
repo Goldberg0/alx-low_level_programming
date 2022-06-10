05fa160e458de105673d2a737f4beda3dea7ed94
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-10 03:29:46 +0100

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints highest prime of 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 2;
+	long n = 612852475143;
+
+
+	while (i < n)
+	{
+		while ((n % i == 0) && (n != i))
+		i++;
+
+	}
+
+	printf("%lu\n", n);
+	return (0);
+}
