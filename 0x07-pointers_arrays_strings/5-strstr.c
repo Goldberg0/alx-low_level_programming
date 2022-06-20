3fdf6b4939404a10470f475269a2546799711c63
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-20 23:14:29 +0100

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search for needle in
+ * @needle: substring to find in haystack
+ * Return: pointer to beginning of needle in haystack or NULL if no match
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+  unsigned int j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+      {
+				break;
+      }
+		}
+		if (needle[j])
+			i++;
+			j = 0;
+		else
+    {
+			return (haystack + i);
+    }
+	}
+	return (0);
+}
