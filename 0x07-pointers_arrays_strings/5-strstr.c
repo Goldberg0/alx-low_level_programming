5c25cd4f4965a09438e71728cef2ea6f53e4f2fe
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-21 00:32:47 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,7 +10,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0;
-  unsigned int j = 0;
+	unsigned int j = 0;
 
 	while (haystack[i])
 	{
@@ -19,17 +19,17 @@ char *_strstr(char *haystack, char *needle)
 			if (haystack[i + j] == needle[j])
 				j++;
 			else
-      {
+			{
 				break;
-      }
+			}
 		}
 		if (needle[j])
 			i++;
 			j = 0;
 		else
-    {
+		{
 			return (haystack + i);
-    }
+		}
 	}
 	return (0);
 }
