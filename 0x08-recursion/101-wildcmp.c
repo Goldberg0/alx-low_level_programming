413e2742708a09a67545fe81df1691daaaed54d2
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-23 02:30:10 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,9 +14,12 @@ int wildcmp(char *s1, char *s2)
 		if (*s2 != '\0' && *s2 == '*')
 			return (wildcmp(s1, s2 + 1));
 		return (*s2 == '\0');
+
 	}
 	if (*s1 != *s2)
+	{
 		return (0);
+	}
 	if (*s2 == '*')
 		return (wildcmp(s2, s1 + 1) || wildcmp(s1, s2 + 1));
 	if (*s1 == *s2)
