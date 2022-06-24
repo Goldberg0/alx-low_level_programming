0ea87a159255b67ca1a4b16bdd3d4512517ba4f9
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-25 00:14:19 +0100

    Create 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#winclude <stdio.h>
+#include <stdlib.h>
+
+/**
+ * change - gives the difference between input and n
+ * @chg : change value
+ * Return: min num of coins
+ */
+int change(int chg)
+{
+	int dough = 0;
+
+	while (chg)
+	{
+		if (chg >= 25)
+			chg -= 25;
+		else if (chg >= 10)
+			chg -= 10;
+		else if (chg >= 5)
+			chg -= 5;
+		else if (chg >= 2)
+			chg -= 2;
+		else if (chg >= 1)
+			chg -= 1;
+		dough++;
+	}
+	return (dough);
+}
+
+/**
+ * main - prints the difference
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful
+ */
+int main(int argc, char **argv)
+{
+	int diff;
+  
+	diff = atoi(argv[1]);
+	if (diff < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+  
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (0);
+  }
+	printf("%i\n", change(diff));
+	return (0);
+}
