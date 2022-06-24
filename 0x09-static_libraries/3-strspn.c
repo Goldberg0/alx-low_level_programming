965c30c54e8e3fa31e41739e9f7e7f0aeb94fece
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-24 18:50:01 +0100

    Create 3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: string to check
+ * @accept: bytes to be accepted
+ * Return: number of bytes in initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, b;
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (s[a] == accept[b])
+				break;
+		}
+		if (!accept[b])
+			break;
+	}
+	return (a);
+}
