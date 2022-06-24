f9b0a1efd4558c4ffc2c46bec19702c57e7519d2
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-24 18:46:58 +0100

    Create 1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - returns length of a string
+* _strncat - concatenates two strings
+* @dest: string to be appended
+* @src: pointer to a string
+* @n: number of elements to append
+* Return: concatenated string
+*/
+
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str)
+	{
+		str++;
+		length++;
+	}
+
+	return (length);
+
+}
+
+
+/* _strncat - concatenates two strings */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *append = dest + _strlen(dest);
+	int length;
+
+	if (n > _strlen(src) + _strlen(dest))
+		length =  _strlen(dest) + _strlen(src);
+	else
+		length = _strlen(dest) + n;
+
+	while (*src && n > 0)
+	{
+		*append += *src;
+		src++;
+		append++;
+		n--;
+	}
+	if (n > 0)
+	*append += '\0';
+	append -= (length);
+	*dest = *append;
+
+	return (append);
+}
