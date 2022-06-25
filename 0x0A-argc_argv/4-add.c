0cf0ead15907063142a8e7adfb890d51f63167c4
Obed Doodo <105594166+Goldberg0@users.noreply.github.com>
2022-06-25 10:04:46 +0100

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ * Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+        return (printf("Error\n"), 1);
+      sum += atoi(argv[i]);
+    }
+	printf("%i\n", sum);
+	return (0);
+}
