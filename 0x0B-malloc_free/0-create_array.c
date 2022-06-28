4731ac02d4af687acaaa648e0a1a145b63dc9888
Obed <ttobed@yahoo.com>
2022-06-28 00:42:29 -0700

    Fixed issues

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
     char *str;
 
     str = malloc(sizeof(char) * size);
-    if (size == 0 || str[i] != c)
+    if (size == 0 || !str)
     {
         return (NULL);
     }
